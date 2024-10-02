46637db087c1d87308cdb25ff25bb9d397caddda
Joaquin Carbonell <joaquin.carbonell@babelgroup.com>
2024-10-02 11:00:21 +0200

    reandomize podiction class names

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,15 +1,18 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { StylesProvider } from "@material-ui/core/styles";
+import { StylesProvider, createGenerateClassName } from "@material-ui/core/styles";
 
 import Landing from "./components/Landing";
 import Pricing from "./components/Pricing";
 
+const generateClassName = createGenerateClassName({
+  productionPrefix: "ma",
+})
+
 const App = () => {
   return (
     <div>
-    
-      <StylesProvider >
+      <StylesProvider generateClassName={generateClassName} >
         <Router>
           <Switch>
             <Route path="/pricing" component={Pricing} />
